6aa425ccf8bb9cfcfdcff6b7b6518d0d7be5feaa
diegoquez <diegoquez@seez.co>
2023-11-20 00:22:10 -0300

    add mfcc representation as parameter

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -4,23 +4,24 @@ import numpy as np
 from typing import Literal
 from datasets import load_dataset, Audio
 from torch.utils.data import Dataset
-from torch.nn.utils.rnn import pad_sequence
 from src.data.vocab import SimpleVocab
 
 MAX_FRAMES = 200
 
+
 class CommonVoice(Dataset):
     def __init__(
-        self, split: Literal["train", "validation", "test"], streaming: bool = False
+        self, split: Literal["train", "validation", "test"], use_mfcc: bool = False
     ):
         self.dataset = load_dataset(
             "mozilla-foundation/common_voice_11_0",
             "en",
             split=split,
-            streaming=streaming,
+            streaming=False,
         )
 
-        #self.dataset = self.dataset.select(range(100))
+        self.dataset = self.dataset.select(range(100))
+        self.use_mfcc = use_mfcc
         self._preprocess()
         self.vocab = SimpleVocab(self.dataset, max_size=10000)
 
@@ -28,17 +29,18 @@ class CommonVoice(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, idx):
+
         item = self.dataset[idx]
         x = torch.tensor(item["audio"]["array"], dtype=torch.float32)
-        x = librosa.feature.mfcc(y=x.numpy(), sr=16_000, n_mfcc=13)
         y = self.vocab.encode(item["sentence"])
 
-        # Pad the MFCCs if necessary
-        pad_width = MAX_FRAMES - x.shape[1]
-        if pad_width > 0:
-            x = np.pad(x, pad_width=((0, 0), (0, pad_width)), mode='constant')
-        elif pad_width < 0:
-            x = x[:, :MAX_FRAMES]
+        if self.use_mfcc:
+            x = librosa.feature.mfcc(y=x.numpy(), sr=16_000, n_mfcc=13)
+            pad_width = MAX_FRAMES - x.shape[1]
+            if pad_width > 0:
+                x = np.pad(x, pad_width=((0, 0), (0, pad_width)), mode='constant')
+            elif pad_width < 0:
+                x = x[:, :MAX_FRAMES]
 
         return x, torch.tensor(y, dtype=torch.long)
 
